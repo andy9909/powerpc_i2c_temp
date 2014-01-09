72f21177473d99ddb66e138acca9a409fe06d420
housir <houwentaoff@gmail.com>
2014-01-09 16:02:20 +0800

    "删除大部分不需要上交的文件，净化版本"

--- update/mkroot.sh
+++
@@ -1,5 +0,0 @@
-#/bin/sh
-genext2fs -N 4096 -b 65536 -d rootfs ramdisk
-gzip  -9  -f  ramdisk
-mkimage -n 'uboot ext2 ramdisk rootfs' -A ppc -O linux -T ramdisk -C gzip -d ramdisk.gz rootfs.ext2.gz
-cp rootfs.ext2.gz /home/lgh
